230e2d51a0d5cd612c8f66ec647b08ed48905fba
hijeong_ <gmlwjdchl82@naver.com>
2022-08-12 23:50:24 +0900

    before redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { GlobalStyle } from "./GlobalStyle";
 import { ShoppingCart } from "./ShoppingCart";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <ShoppingCart />
     </>
   );
--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Wrap = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
@@ -13,40 +14,76 @@ const Items = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 50%;
+  width: 100%;
+  padding: 10px 120px;
+  box-sizing: border-box;
 `;
+
+const Head = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 20px 120px;
+  box-sizing: border-box;
+  background-color: lightblue;
+`;
+const Name = styled.h3``;
+
+const TWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const Check = styled.div``;
+const All = styled.div``;
+const Delete = styled.div``;
 
 const Btn = styled.button`
   all: unset;
 `;
 
-const Cart = styled.div`
-  width: 50%;
-`;
-
 export const ShoppingCart = () => {
-  const handleClick = (e) => {
-    const item = console.log(item);
-  };
+  const itemList = [
+    {
+      id: 0,
+      name: "곰인형",
+      price: 10000,
+    },
+    {
+      id: 1,
+      name: "곰인형",
+      price: 10000,
+    },
+    {
+      id: 2,
+      name: "곰인형",
+      price: 10000,
+    },
+  ];
+
   return (
     <Wrap>
+      <Head>
+        <Name>선택</Name>
+        <Name>상품명</Name>
+        <Name>가격</Name>
+        <Name>수량</Name>
+        <Name>합계</Name>
+        <Name>삭제</Name>
+      </Head>
       <Items>
-        <Btn value="1" onClick={handleClick}>
-          item 1
-        </Btn>
-        <Btn value="2" onClick={handleClick}>
-          item 2
-        </Btn>
-        <Btn value="3" onClick={handleClick}>
-          item 3
-        </Btn>
-        <Btn value="4" onClick={handleClick}>
-          item 4
-        </Btn>
+        {itemList.map((item) => (
+          <TWrap>
+            <Check>X</Check>
+            <h1>{item.name}</h1>
+            <h1>{item.price}</h1>
+            <Btn>+</Btn>
+            <All>-</All>
+            <Delete>X</Delete>
+          </TWrap>
+        ))}
       </Items>
-      <Cart>
-        카트<ul></ul>
-      </Cart>
     </Wrap>
   );
 };
